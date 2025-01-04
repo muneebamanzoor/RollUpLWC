b68dacc30ee795ada245c8f99ae6d2f90a53e024
Muneeba Manzoor <61382726+muneebamanzoor@users.noreply.github.com>
2025-01-04 22:44:52 +0500

    Update rollupIndicator.js to include dynamic label and value of required rollup field

--- force-app/main/default/lwc/rollupIndicator/rollupIndicator.js
+++ force-app/main/default/lwc/rollupIndicator/rollupIndicator.js
@@ -1,54 +1,29 @@
 import { LightningElement, api, wire } from 'lwc';
-import getRollupFieldValue from '@salesforce/apex/RollupIndicatorController.getRollupFieldValue';
+import getRollupFieldDetails from '@salesforce/apex/RollupIndicatorController.getRollupFieldDetails';
 
 export default class RollupIndicator extends LightningElement {
     @api recordId; 
-    @api targetObject;
-    @api rollupFieldApiName;
+    @api targetObject; 
+    @api rollupFieldApiName; 
 
-    rollupValue; 
-    error;
-
-    @wire(getRollupFieldValue, {
+    rollupFieldLabel; 
+    rollupFieldValue;
+    error; 
+    
+    @wire(getRollupFieldDetails, {
         targetObject: '$targetObject',
         rollupFieldApiName: '$rollupFieldApiName',
         recordId: '$recordId'
     })
     wiredRollupField({ error, data }) {
         if (data) {
-            this.rollupValue = data;
+            this.rollupFieldLabel = data.fieldLabel; 
+            this.rollupFieldValue = data.fieldValue; 
             this.error = undefined;
         } else if (error) {
             this.error = error.body.message;
-            this.rollupValue = undefined;
+            this.rollupFieldLabel = undefined;
+            this.rollupFieldValue = undefined;
         }
     }
 }
-
-
-// import { LightningElement, api, wire } from 'lwc';
-// import getRollupFieldValue from '@salesforce/apex/RollupIndicatorController.getRollupFieldValue';
-
-// export default class RollupIndicator extends LightningElement {
-//     @api recordId;
-//     @api targetObject = 'Account';
-//     @api rollupFieldApiName = 'AnnualRevenue';
-
-//     rollupValue;
-//     error;
-
-//     @wire(getRollupFieldValue, {
-//         targetObject: '$targetObject',
-//         rollupFieldApiName: '$rollupFieldApiName',
-//         recordId: '$recordId'
-//     })
-//     wiredRollupField({ error, data }) {
-//         if (data) {
-//             this.rollupValue = data;
-//             this.error = undefined;
-//         } else if (error) {
-//             this.error = error.body.message;
-//             this.rollupValue = undefined;
-//         }
-//     }
-// }
